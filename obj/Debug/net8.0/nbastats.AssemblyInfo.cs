227e96a430e05a7c34f59574fd67c36160ed87f8
Brady Zalasky <bzalasky@iastate.edu>
2024-11-08 08:13:05 -0800

    rider git test

--- obj/Debug/net8.0/nbastats.AssemblyInfo.cs
+++ obj/Debug/net8.0/nbastats.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nbastats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14bcf74a092658d85b759284ae34929c9324513b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20313c32dde6d86e119f1ce28f8035343423be7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("nbastats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nbastats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
